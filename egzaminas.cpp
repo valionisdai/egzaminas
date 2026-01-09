913b3d9cd45254a8175af90512b0e8c62cb787f0
Dainius <dainius1120@gmail.com>
2026-01-09 13:44:12 +0200

    Implement word counting and line tracking from file
    The program reads words from 'tekstas.txt', counts their occurrences, and records the lines they appear on. It then writes the results to 'rez.txt'.

---
+++ egzaminas.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+#include <iomanip>
+#include <cctype>
+#include <algorithm>
+
+
+
+using namespace std;
+
+class zodzio_info {
+    public:
+        vector<int> eilutes;
+        int kiekis = 0;
+};
+
+string valymas (string zodis);
+
+int main() {
+
+    unordered_map<string, zodzio_info> p_zodziai;
+  
+    ifstream rd("tekstas.txt");
+    string word, rez;
+    if (!rd.is_open()) {
+        cerr << "Nepavyko atidaryti failo!" << endl;
+    }
+
+    string line, zodis;
+    int eilnr = 0;
+    
+    while(getline(rd, line))
+    {
+        eilnr++;
+        istringstream iss(line);
+        while (iss >> zodis)
+        {
+            zodis = valymas(zodis);
+            if (zodis.empty()) continue;
+            auto & info = p_zodziai[zodis];
+            info.kiekis++;
+            if (info.eilutes.empty() || info.eilutes.back() != eilnr)
+                info.eilutes.push_back(eilnr);
+        }
+    }
+
+    ofstream wr("rez.txt");
+    wr << left << setw(25) << "Zodis" << setw(8) << "Kiekis" << "Eilutes" << endl;
+    wr << string(60, '-') << endl;
+
+    for (auto& i : p_zodziai) {
+        if (i.second.kiekis > 1) 
+        {
+            wr << left << setw(25) << i.first << setw(8) << i.second.kiekis;
+            for (auto eil : i.second.eilutes)
+                wr << eil << " ";
+            wr << endl;
+        }
+    }
+    return 0;
+}
+
+string valymas (string zodis)
+{
+    zodis.erase(remove_if(zodis.begin(), zodis.end(), [](unsigned char c) { return ispunct(c); }), zodis.end());
+    zodis.erase(zodis.begin(), find_if(zodis.begin(), zodis.end(), [](unsigned char c){ return !isspace(c); }));
+    zodis.erase(find_if(zodis.rbegin(), zodis.rend(), [](unsigned char c){ return !isspace(c); }).base(), zodis.end());   
+    transform(zodis.begin(), zodis.end(), zodis.begin(), ::tolower);
+    return zodis;
+}
